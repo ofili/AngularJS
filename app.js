fb18cad2efdd74d01be2aef33b1ae152d0a7a9cf
Lewis <ofili@users.noreply.github.com>
2019-05-27 21:34:41 +0100

    js

--- app.js
+++ app.js
@@ -7,6 +7,19 @@
         $scope.name = "";
         $scope.totalValue = 0;
 
+        $scope.displayNumeric = function() {
+            var totalnameValue = calculateNumericForString($scope.name);
+            $scope.totalValue = totalnameValue;
+        };
+
+        function calculateNumericForString(tring) {
+            var totalStringValue = 0;
+            for (var i = 0; i < String.length; i++) {
+                totalStringValue += String.charCodeAt(i);
+            }
+            return totalStringValue;
+        };
+
     });
 
 
